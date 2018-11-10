19df4225c16b844948886d30ddaac72d9df35202
Fernando J. Recci <reccifernando@gmail.com>
2018-11-09 23:14:12 -0300

    commit inicial

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from trytond.pool import Pool
+from .partner import *
+from .meeting import *
+from .vacation import *
+from .partnermeeting import *
+from .sanction import *
+from .recibo import Recibo, LineaConceptoRecibo, ReciboReport
+from .account import *
+from .company import *
+
+
+def register():
+    Pool.register(Partner,
+                  Meeting,
+                  Vacation,
+                  PartnerMeeting,
+                  Sanction,
+                  Recibo,
+                  LineaConceptoRecibo,
+                  FiscalYear,
+                  Company,
+                  module='cooperative_ar', type_='model'
+                  )
+    Pool.register(
+        ReciboReport,
+        module='cooperative_ar', type_='report')
